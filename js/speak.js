adceac93a3041afc53029949024e2a8fe6a8fd10
gegenisang <5127744145@qq.com>
2017-09-06 10:34:11 +0800

    search 完成

--- js/speak.js
+++ js/speak.js
@@ -1,19 +1,36 @@
 $(document).ready(function () {
+  var clear = false;
   var oInp = $(".search-form input").val();
   $(".search-form input").focus(function () {
     $(this).parent().addClass("focus");
     $(".clear-btn").addClass("focus").removeClass("hide");
   });
-  $(".clear-btn").click(function (e) {
-    console.log('e1', e);
-    $(".search-form input").val(oInp);
-    $(".search-form input").focus();
-  });
-  $(".search-form input").blur(function (e) {
-    console.log('e2', e);
-    $(this).parent().removeClass("focus");
+
+  function clearClass(obj) {
+    $(obj).parent().removeClass("focus");
     $(".clear-btn").removeClass("focus").addClass("hide");
+  }
+  $(".clear-btn").click(function () {
+    var value = $(".search-form input").val();
+    clear = true;
+    if (clear && value !== oInp) {
+      $(".search-form input").val(oInp);
+      $(".search-form input").focus();
+    } else if (clear && value === oInp) {
+      clearClass(this);
+      $(".search-form input").val(oInp);
+      clear = false;
+    }
   });
+  $(".search-form input").blur(function () {
+    var value = $(".search-form input").val();
+    if (!clear && value === oInp) {
+      clearClass(this);
+    } else if (clear && value === oInp) {
+      clearClass(this);
+    }
+  });
+
 
 
 });
