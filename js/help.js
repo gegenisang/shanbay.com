f32d1fe37ab63a04ee288ac132b41c807432d2ae
gegenisang <5127744145@qq.com>
2017-09-11 22:18:59 +0800

    complete help all

--- js/help.js
+++ js/help.js
@@ -1,7 +1,10 @@
 $(".list>a").click(function () {
-  $(this).addClass("active1")
-    .next().show()
-    .parent().siblings().children("a").removeClass("active1")
-    .next().hide();
+  $(this).next().show().parent(".list").addClass("active1")
+    .siblings().removeClass("active1").children("a").next().hide();
   return false;
+});
+
+$(".list ul li").click(function () {
+  $(".list ul li").removeClass("active");
+  $(this).addClass("active");
 });
