64ce8dccdfcd9cad3598659c09c2ce4503b49b94
gegenisang <gegenisang@gmail.com>
2017-09-14 14:46:21 +0800

    register

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var htmlmin = require('gulp-htmlmin');
+
+var paths = {
+    src: "src",
+    sass: "src/sass",
+    dist: "dist"
+
+}
+gulp.task('img', function() {
+    return gulp.src(paths.src + "/img/**/*")
+        .pipe(gulp.dest(paths.dist + "/img"));
+});
+gulp.task("sass", function() {
+    return gulp.src(paths.sass + "/**/*.scss")
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest(paths.dist + '/css'));
+});
+gulp.task("html", function() {
+    return gulp.src(paths.src + "/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(paths.sass + "/**/*.scss", ["sass"]);
+    gulp.watch(paths.src + "/**/*.html", ['html']);
+    gulp.watch(paths.src + "/img/**/*.*", ["img"]);
+});
