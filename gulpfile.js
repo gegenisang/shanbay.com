eed64f1d2f1477945f0772d43db7fdb34260ae8a
gegenisang <gegenisang@gmail.com>
2017-09-15 22:27:42 +0800

    my_news page not complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,62 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var htmlmin = require('gulp-htmlmin');
+var browserSync = require('browser-sync').create()
 
 var paths = {
     src: "src",
     sass: "src/sass",
     dist: "dist"
 
-}
-gulp.task('img', function() {
+};
+
+
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: paths.dist
+        },
+    });
+});
+
+
+
+gulp.task('img', function () {
     return gulp.src(paths.src + "/img/**/*")
         .pipe(gulp.dest(paths.dist + "/img"));
 });
 
 
-gulp.task('lib', function() {
+gulp.task('lib', function () {
     return gulp.src("bower_components/**/*")
         .pipe(gulp.dest(paths.dist + "/bower_components"));
 });
 
-gulp.task("sass", function() {
+gulp.task("sass", function () {
     return gulp.src(paths.sass + "/**/*.scss")
         // .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest(paths.dist + '/css'));
 });
 
-gulp.task("html", function() {
+gulp.task("html", function () {
     return gulp.src(paths.src + "/*.html")
-        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
         .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch(paths.sass + "/**/*.scss", ["sass"]);
     gulp.watch(paths.src + "/**/*.html", ['html']);
     gulp.watch(paths.src + "/img/**/*.*", ["img"]);
+
+    gulp.watch(paths.dist + '/**/*').on('change', browserSync.reload);
+
 });
+
+gulp.task('auto', ['browserSync', 'watch']);
+
+gulp.task('default', ['auto']);
